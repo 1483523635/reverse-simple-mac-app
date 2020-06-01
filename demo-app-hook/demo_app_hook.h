1ab3acae3a62c96d7db58c4e55324dee5fb8612e
XiaoQu <lqqu@thoughtworks.com>
2020-06-01 09:31:54 +0800

    Initial Commit

---
+++ demo-app-hook/demo_app_hook.h
@@ -0,0 +1,19 @@
+//
+//  demo_app_hook.h
+//  demo-app-hook
+//
+//  Created by Lianqing Qu  on 6/1/20.
+//  Copyright © 2020 Lianqing Qu . All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for demo_app_hook.
+FOUNDATION_EXPORT double demo_app_hookVersionNumber;
+
+//! Project version string for demo_app_hook.
+FOUNDATION_EXPORT const unsigned char demo_app_hookVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <demo_app_hook/PublicHeader.h>
+
+
